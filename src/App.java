4e6bb8613952ee194054c54fcbb878730e27fedf
Rodrigo Colozio <rodrigocolozio@gmail.com>
2022-12-21 14:51:16 -0300

    project to add a increasemnt in a emplooye's salary via ArraList and based on its id number.

---
+++ src/App.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import org.omg.PortableServer.IdAssignmentPolicy;
+
+import entities.Employee;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+            List<Employee> list = new ArrayList<>();
+
+            // inicio 
+            System.out.print("How many employees will be registered? ");
+            int n = sc.nextInt();
+
+            for (int i = 0; i<n; i++){
+                System.out.println("Employee #" + (i+1) + ":");
+                System.out.print("Id: ");
+                Integer id= sc.nextInt();
+                System.out.print("Name: ");
+                sc.nextLine();
+                String name = sc.nextLine();           
+                System.out.print("Salary: ");
+                Double salary = sc.nextDouble();
+
+                Employee emp = new Employee(id, name, salary);
+
+                list.add(emp);
+               
+                
+            }
+
+            // increase Salary option
+
+            System.out.print("Enter the employee id that will have salary increase: ");
+            int idSalary = sc.nextInt();
+            Integer pos = position(list, idSalary);
+            if (pos == null){
+                System.out.println("This is does not exist!");
+            }
+            else{
+                System.out.print("Enter the percentage: ");
+                double percent = sc.nextDouble();
+                list.get(pos).increaseSalary(percent);
+            }
+            System.out.println();
+            System.out.println("List of employees: ");
+            for(Employee emp : list){
+                System.out.println(emp);
+            }
+
+        sc.close();
+    }
+
+    public static Integer position (List<Employee> list, int id){
+        for(int i = 0; i<list.size(); i++){
+            if(list.get(i).getId() == id){
+                return i;
+            }
+        }
+        return null;
+    }
+
+}
